5ad2779758ff7798806e27823869a70d60d215f4
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-01-31 18:24:10 +0530

    Initial commit: Added day6 folder with main.py

---
+++ 06-Day6-Variables-and-Data-Types/main.py
@@ -0,0 +1,20 @@
+a = complex(8, 2)
+print(a)
+b = "Rohit Jiwade"
+print(b)
+c = True
+d = None
+
+print("The type of a is", type(a))
+print("The type of a is", type(b))
+print("The type of a is", type(c))
+print("The type of a is", type(d))
+
+list = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list)
+
+tuple = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple)
+
+dict = {"name": "Rohit", "age": 20, "canVote": True}
+print(dict)
