c8e4cf1f1e5538fb51cd8c20c3d1bc44f16ad3c9
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-01-31 18:20:52 +0530

    Initial commit: Added day5 folder with main.py

---
+++ 05-Day5-Comments-and-Print/main.py
@@ -0,0 +1,19 @@
+
+# single-line Comments
+
+"""
+
+Multiline-Comments
+
+Hey Hey
+Hey
+Hey
+Hey
+Hey
+"""
+print('Hey I am\' a \"good\" boy\nand this viewer is also a good boy/girl')
+
+print('Hello World')
+
+print("Hi", 6, 7, sep="~", end="009\n")
+print("he")
