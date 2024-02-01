381f34b420f3e2b4e56df5a0215b2ddafa7da60d
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-01 15:47:39 +0530

    Initial commit: Added day8 folder with main.py

---
+++ 08-Day8-Exercise-1-Create-a-Calculator-Solution/main.py
@@ -0,0 +1,10 @@
+a = 12
+b = 5
+
+print("The Value of", a, "+", b, "is  : ",a + b)
+print("The Value of", a, "-", b, "is  : ",a - b)
+print("The Value of", a, "*", b, "is  : ",a * b)
+print("The Value of", a, "/", b, "is  : ",a / b)
+print("The Value of", a, "%", b, "is  : ",a % b)
+print("The Value of", a, "**", b, "is : ",a ** b)
+print("The Value of", a, "//", b, "is : ",a // b)
