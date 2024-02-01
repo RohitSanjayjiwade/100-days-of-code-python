efc884028c31caecedd176d511a3b4b99824de3f
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-01 15:49:44 +0530

    Initial commit: Added day9 folder with main.py

---
+++ 09-Day9-Typecasting-in-Python/main.py
@@ -0,0 +1,26 @@
+a = "1"
+# a = 1
+b = "2"
+# b = 2
+print(int(a)+int(b))
+
+
+# Implicit Typecasting
+c = 1.9
+d = 8
+print(c + d)
+
+# Python autimatical convert
+# a to int
+a = 7
+print(type(a))
+
+# Python automatical convert b to float
+b = 3.0
+print(type(b))
+
+# python autimatical convert c to float as it is a float addition
+c = a + b
+print(c)
+
+print(type(c))
