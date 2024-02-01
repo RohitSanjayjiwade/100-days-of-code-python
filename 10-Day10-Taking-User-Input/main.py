990f2300f2b348446b04b5f954afc4492def22c7
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-01 15:55:52 +0530

    Initial commit: Added day10 folder with main.py

---
+++ 10-Day10-Taking-User-Input/main.py
@@ -0,0 +1,30 @@
+a = input("Enter Your Name : ")
+print("My Name is ", a)
+print(type(a))
+
+x = input("Enter First Number: ")
+y = input("Enter Second Number : ")
+print(x + y)
+print(int(x) + int(y))
+
+x = int(input("Enter First Number: "))
+y = int(input("Enter Second Number : "))
+print(x + y)
+
+
+
+
+
+# output : 
+# 	a = input("Enter Your Name : ")
+# 	print("My Name is ", a)
+# 	print(type(a))
+
+# 	x = input("Enter First Number: ")
+# 	y = input("Enter Second Number : ")
+# 	print(x + y)
+# 	print(int(x) + int(y))
+
+# 	x = int(input("Enter First Number: "))
+# 	y = int(input("Enter Second Number : "))
+# 	print(x + y)
