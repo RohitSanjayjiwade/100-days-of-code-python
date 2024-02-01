751960531adad67513fae227c798305b5597adbf
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-01 18:58:16 +0530

    Initial commit: Added day11 folder with main.py

---
+++ 11-Day11-Strings/main.py
@@ -0,0 +1,23 @@
+name = "Rohit"
+print("Hello", name)
+
+friend = "Rohan"
+anotherFriend = 'Tushar'
+
+print("Hello", friend)
+print("Hello", anotherFriend)
+
+apple = '''He Said
+is Not
+Working'''
+
+print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+# print(name[5])  Throws an error
+
+for character in apple:
+  print(character)
