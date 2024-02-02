1b86fb453d38154635731d295b5217aa5276ec8e
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-02 12:02:19 +0530

    Initial commit: Added day13 folder with main.py

---
+++ 13-Day13-String-Methods/main.py
@@ -0,0 +1,58 @@
+# String are immutable
+a = "Rohit!!!! !!!!! Rohit"
+print(len(a))
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.strip())
+print(a.rstrip("!"))
+print(a.replace("Rohit", "Chetan"))
+print(a.split(" "))
+
+blogHeading = "introduction tO jS"
+print(blogHeading.capitalize())
+
+str1 = "Welcome to the Console!!!"
+print(len(str1))
+print(len(str1.center(50)))
+
+print(a.count("Rohit"))
+print(str1.endswith("!!!"))
+print(str1.endswith("to", 4, 10))
+
+str1 = "He's name is Dan. He is an honest man,"
+print(str1.find("is"))
+print(str1.find("rohit"))
+# print(str1.index("rohit"))
+
+str1 = "WelcomeToTheConsole00"
+print(str1.isalnum())
+
+str1 = "Welcome00"
+print(str1.isalpha())
+
+str1 = "hello world"
+print(str1.islower())
+
+str1 = "We wish you a Merry Christmas\n"
+print(str1.isprintable())
+
+str1 = "        "  #using Spacebar
+print(str1.isspace())
+str2 = "        "  #using Tab
+print(str2.isspace())
+
+str1 = "World Health Organisation"
+print(str1.istitle())
+
+str1 = "ERRRRf"
+print(str1.isupper())
+
+str1 = "Python is a Interpreted Language"
+print(str1.startswith("Python"))
+
+str1 = "Python is a Interpreted Language"
+print(str1.swapcase())
+
+str1 = "His name is Dan. Dan is an honest man."
+print(str1.title())
