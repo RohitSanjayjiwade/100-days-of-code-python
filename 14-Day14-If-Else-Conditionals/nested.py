a5c1d8a0ddb1bd9c885518a10af24b674bca7c7a
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-03 11:11:35 +0530

    Added day14 folder with nested.py

---
+++ 14-Day14-If-Else-Conditionals/nested.py
@@ -0,0 +1,27 @@
+num = 18
+if (num < 0):
+  print("Number is negative.")
+elif (num > 0):
+  if (num <= 10):
+    print("Number is between 1-10")
+  elif (num > 10 and num <= 20):
+    print("Number is between 11-20")
+  else:
+    print("Number is greater than 20")
+else:
+  print("Number is zero")
+
+# --------------------------------------------------
+# a = 10
+# b = 20
+# c = 30
+
+# if (a > b):
+#   if (a > c):
+#     print("a is greater")
+#   else:
+#     print("c is greater")
+# if (b > c):
+#   print("b is greater")
+# else:
+#   print("c is greater")
