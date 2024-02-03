bb1e97e57e37b1094f32784231ea56a9736d7c3a
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-03 13:31:03 +0530

    Added day17 folder with main.py

---
+++ 17-Day17-For-Loops/main.py
@@ -0,0 +1,37 @@
+# name  = "Rohit"
+# for i in name:
+#   print(i)
+#   if(i == "o"):
+#     print("This is something special")
+
+
+
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for color in colors:
+#   print(color)
+#   for i in color:
+#     print(i)
+
+# for i in range(5):
+#   print(i+1)
+
+# for i in range(1,101,2):
+#   print(i+1)
+
+# fact = 1
+# for i in range(1,5+1):
+#   fact = fact*i
+# print(fact)
+
+
+a = 0 
+b = 1
+print(a)
+print(b)
+for i in range(6+1):
+  c = a+b
+  print(c, end=", ")
+  a = b
+  b = c
+
+  
