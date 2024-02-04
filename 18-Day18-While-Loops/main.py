97d0d9f98d480d03bd3adcd973687aaa954849ec
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-04 20:21:06 +0530

    Added day18 folder with main.py

---
+++ 18-Day18-While-Loops/main.py
@@ -0,0 +1,17 @@
+# i = 1
+# while(i<=10):
+#   print(i)
+#   i = i+1
+
+# i = 10
+# while(i>=1):
+#   print(i)
+#   i = i-1
+
+i = int(input("Enter the number: "))
+print(i)
+while(i<=38):
+  i = int(input("Enter the number: "))
+  print(i)
+else:
+  print("Done with the loop")
