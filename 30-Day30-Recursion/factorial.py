f31c34fcbc077bb57cec4501422a12200ff0d1dc
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-07 17:23:24 +0530

    Added day30 folder with factorial.py

---
+++ 30-Day30-Recursion/factorial.py
@@ -0,0 +1,14 @@
+#  Facorial of a number using Recursion
+
+def fact(n):
+  # print(n)
+  if n == 0 or n == 1:
+    return 1
+  else:
+    total =  n * fact(n-1)
+    return total
+
+
+n = int(input("Enter a number: "))
+a = fact(n)
+print(a)
