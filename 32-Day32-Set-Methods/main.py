2fd9e715eb723a85dd44373d95bceab4affbeb95
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-08 20:41:44 +0530

    Added day32 folder with main.py

---
+++ 32-Day32-Set-Methods/main.py
@@ -0,0 +1,76 @@
+s = {1, 2, 5, 6}
+s2 = {3, 6, 7}
+print(s.union(s2))
+print(s)
+s.update({10})
+s.update(s2)
+print(s, s2)
+
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+# cities3 = cities.union(cities2)
+# print(cities3)
+
+# cities3 = cities.intersection(cities2)
+# print(cities3)
+
+# cities.intersection_update(cities2)
+# print(cities)
+
+# cities3 = cities.symmetric_difference(cities2)
+# print(cities3)
+
+# cities.symmetric_difference_update(cities2)
+# print(cities)
+# print(cities2)
+
+# cities3 = cities.difference(cities2)
+# print(cities3)
+
+# cities.difference_update(cities2)
+# print(cities)
+
+# print(cities.isdisjoint(cities2))
+
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Seoul", "Kabul"}
+# print(cities.issuperset(cities2))
+# cities3 = {"Seoul", "Madrid","Kabul"}
+# print(cities.issuperset(cities3))
+
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Delhi", "Madrid"}
+# print(cities2.issubset(cities))
+
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities.add("Helsinki")
+# print(cities)
+
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities.remove("Tokyo")
+# print(cities)
+
+# cities.discard("Tokyo2")
+# print(cities)
+
+cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+item = cities.pop()
+print(cities)
+print(item)
+
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# del cities
+# print(cities)
+
+cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+cities.clear()
+print(cities)
+
+info = {"Carla", 19, False, 5.9}
+if "Carla" in info:
+  print("Carla is present.")
+else:
+  print("Carla is absent.")
+
+info.add(1)
+print(info)
