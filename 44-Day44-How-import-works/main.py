c1cb03faf9f72c2d54c815fb631a7d77cb310d7a
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-16 17:46:54 +0530

    Added day44 folder with main.py and rohit.py

---
+++ 44-Day44-How-import-works/main.py
@@ -0,0 +1,38 @@
+# import pandas as pd
+# import math
+# import math as m
+# from math import *
+# from math import pi, sqrt as s
+# import math
+import rohit
+
+# pd.read_csv('data.csv')
+
+# total = math.sqrt(9) * math.pi
+# print(total)
+
+# total = m.sqrt(9) * m.pi
+# print(total)
+
+# total = sqrt(9) * pi
+# print(total)
+
+# total = s(9) * pi
+# print(total)
+
+# print(dir(math))
+# print(math.nan, type(math.nan))
+# print(math.__doc__)
+
+
+rohit.welcome()
+print(rohit.rohit)
+
+print(dir(rohit))
+print(rohit.__doc__)
+print(rohit.welcome.__doc__)
+
+
+
+
+
---
+++ 44-Day44-How-import-works/rohit.py
@@ -0,0 +1,9 @@
+"""This is Rohit Module"""
+
+def welcome():
+  """This is a Welcome message"""
+  print("Hey you are welcome from Rohit")
+
+
+
+rohit = "A good boy"
