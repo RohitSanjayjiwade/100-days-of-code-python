255d3e1d8269a47507855591336df629115c0977
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-02-16 17:46:15 +0530

    Added day43 folder with main.py

---
+++ 43-Day43-Virtual-Environment/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+print(pd.__version__)
+
+
+# touch   is use to create a file
+# ls      is use to list the files
+# mkdir   is use to create a directory
+# rmdir   is use to delete a directory
+# rm      is use to delete a file
+# pip     is use to install a package
+# pip3    is use to install a package
+# pip freeze > requirements.txt   is use to create a requirements.txt file
+# pip install -r requirements.txt   is use to install the packages in requirements.txt file
